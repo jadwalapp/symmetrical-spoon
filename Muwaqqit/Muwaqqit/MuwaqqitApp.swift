bf018c599ae1170291d159fd594fb14485dfb00e
Yazeed AlKhalaf <yazeed@alkhalaf.dev>
2024-09-29 14:09:37 +0300

    create the iOS project :D

---
+++ Muwaqqit/Muwaqqit/MuwaqqitApp.swift
@@ -0,0 +1,17 @@
+//
+//  MuwaqqitApp.swift
+//  Muwaqqit
+//
+//  Created by Yazeed AlKhalaf on 29/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct MuwaqqitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
