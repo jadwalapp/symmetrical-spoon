b1450d7dcf002af4579c4ea472bede35757a1da8
Yazeed AlKhalaf <yazeed@alkhalaf.dev>
2024-10-30 11:56:02 +0300

    try elegant calendar swift

--- mishkat/Mishkat/Features/Calendar/CalendarView.swift
+++ mishkat/Mishkat/Features/Calendar/CalendarView.swift
@@ -6,10 +6,30 @@
 //
 
 import SwiftUI
+import ElegantCalendar
 
 struct CalendarView: View {
+    let startDate = Date().addingTimeInterval(TimeInterval(60 * 60 * 24 * (-30 * 36)))
+    let endDate = Date().addingTimeInterval(TimeInterval(60 * 60 * 24 * (30 * 36)))
+    
+    @ObservedObject var calMan: ElegantCalendarManager
+    
+    init() {
+        calMan = ElegantCalendarManager(
+            configuration: CalendarConfiguration(
+                startDate: startDate,
+                endDate: endDate
+            ),
+            initialMonth: Date()
+        )
+    }
+    
     var body: some View {
-        Text("Calendar View")
+        ElegantCalendarView(
+            calendarManager: calMan
+        )
+        .theme(.kiwiGreen)
+        .vertical()
     }
 }
 
