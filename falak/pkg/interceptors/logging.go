3a0a0deb5b6511262f614e9bc2130629d87d4562
Yazeed AlKhalaf <yazeed@alkhalaf.dev>
2025-01-06 00:16:34 +0300

    add better logging :D

--- falak/pkg/interceptors/logging.go
+++ falak/pkg/interceptors/logging.go
@@ -3,6 +3,7 @@ package interceptors
 import (
 	"context"
 	"os"
+	"time"
 
 	"connectrpc.com/connect"
 	"github.com/rs/zerolog"
@@ -11,10 +12,31 @@ import (
 func LoggingInterceptor() connect.UnaryInterceptorFunc {
 	return func(next connect.UnaryFunc) connect.UnaryFunc {
 		return func(ctx context.Context, req connect.AnyRequest) (connect.AnyResponse, error) {
-			logger := zerolog.New(os.Stderr).With().Timestamp().Str("service", req.Spec().Procedure).Logger()
+			logger := zerolog.New(os.Stderr).With().
+				Timestamp().
+				Str("method", req.Spec().Procedure).
+				Logger()
 			ctx = logger.WithContext(ctx)
 
-			return next(ctx, req)
+			logger.Info().Msg("Request started")
+
+			start := time.Now()
+			resp, err := next(ctx, req)
+			duration := time.Since(start)
+
+			if err != nil {
+				logger.Error().
+					Err(err).
+					Dur("duration", duration).
+					Msg("Request failed")
+				return resp, err
+			}
+
+			logger.Info().
+				Dur("duration", duration).
+				Msg("Request completed")
+
+			return resp, err
 		}
 	}
 }
